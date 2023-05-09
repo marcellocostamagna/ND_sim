12c951fd3611b8e1686bc05069478e18d4148898
Marcello Costamagna <marcello.costamagna@uib.no>
2023-05-09 14:36:14 +0200

    starts implementation with number of protons, neutrons and elctrons

--- similarity_based_on_axis.py
+++ similarity_based_on_axis.py
@@ -6,28 +6,33 @@ import math
 import axis_fingerprint as afp
 from similarity_3d import calculate_partial_score
 import matplotlib.pyplot as plt
+from perturbations import *
 
+
+# The properties should be:
+# 1. Number of protons
+# 2. Number of neutrons
+# 3. Number of electrons
+
+# BIATOMIC MOLECULES
 # point_cloud_1:
 # coordinates
-pc_1 = np.array([
-     [  1,  0, -1/math.sqrt(2)],
-     [ -1,  0, -1/math.sqrt(2)],
-     [  0,  1,  1/math.sqrt(2)],
-     [  0, -1,  1/math.sqrt(2)]
- ])
+pc_1 = np.array([[ 1, 1, 1], [-1, -1, -1] ])
 # masses
-masses_1 = [1, 3, 5, 7 ]
+n_protons = [1, 3 ]
+n_neutrons = [1, 3 ]
+n_electrons = [1, 3 ]
 
 # point_cloud_2:
 # coordinates
-pc_2 = np.array([
-     [  1,  0, -1/math.sqrt(2)],
-     [ -1,  0, -1/math.sqrt(2)],
-     [  0,  1,  1/math.sqrt(2)],
-     [  0, -1,  1/math.sqrt(2)]
- ])
+pc_2 = np.array([[ 1, 1, 1], [-1, -1, -1] ])
 # masses
-masses_2 = [1, 3, 7, 5 ] 
+n_protons = [3, 1]
+n_neutrons = [3, 1]
+n_electrons = [3, 1]
+
+
+
 
 # Fingerprints
 fingerprint_1, mass_weighted_fingerprint_1 = afp.compute_fingerprint(pc_1, masses_1)
@@ -44,3 +49,66 @@ print(f'Mass-weighted similarity: {mass_weighted_similarity}')
 
 
 plt.show()
+
+
+# # BIATOMIC MOLECULES
+# # point_cloud_1:
+# # coordinates
+# pc_1 = np.array([[ 1, 1, 1], [-1, -1, -1] ])
+# # masses
+# masses_1 = [1, 3 ]
+
+# # point_cloud_2:
+# # coordinates
+# pc_2 = np.array([[ 1, 1, 1], [-1, -1, -1] ])
+# # masses
+# masses_2 = [3, 1 ]
+
+# # TRIATOMIC MOLECULES
+# # point_cloud_1:
+# # coordinates
+# pc_1 = np.array([[ 1, 1, 1], [-1, -1, -1], [0, 0, 0] ])
+# # masses
+# masses_1 = [1, 3, 5 ]
+
+# # point_cloud_2:
+# # coordinates
+# pc_2 = np.array([[ 1, 1, 1], [-1, -1, -1], [0, 0, 0] ])
+# # masses
+# masses_2 = [3, 1, 5 ]
+
+# # TRIATOMIC MOLECULES in a Triangle
+# # point_cloud_1:
+# # coordinates
+# pc_1 = np.array([[ 0, 0, 0], [1, 0, 0], [0.5, 0.5*math.sqrt(3) , 0] ])
+# # masses
+# masses_1 = [1, 3, 5 ]
+
+# # point_cloud_2:
+# # coordinates
+# pc_2 = np.array([[ 0, 0, 0], [1, 0, 0], [0.5, 0.5*math.sqrt(3) , 0] ])
+# # masses
+# masses_2 = [3, 1, 5 ]
+
+
+# # TETRAHEDRONS
+# # coordinates
+# pc_1 = np.array([
+#      [  1,  0, -1/math.sqrt(2)],
+#      [ -1,  0, -1/math.sqrt(2)],
+#      [  0,  1,  1/math.sqrt(2)],
+#      [  0, -1,  1/math.sqrt(2)]
+#  ])
+# # masses
+# masses_1 = [1, 3, 5, 7 ]
+
+# # point_cloud_2:
+# # coordinates
+# pc_2 = np.array([
+#      [  1,  0, -1/math.sqrt(2)],
+#      [ -1,  0, -1/math.sqrt(2)],
+#      [  0,  1,  1/math.sqrt(2)],
+#      [  0, -1,  1/math.sqrt(2)]
+#  ])
+# # masses
+# masses_2 = [1, 3, 7, 5 ] 
