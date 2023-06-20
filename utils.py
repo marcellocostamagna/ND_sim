1734ebe1a0eea7bef5fd379c060d84bdac5a861c
Marcello Costamagna <marcello.costamagna@uib.no>
2023-06-20 11:30:26 +0200

    adds tapering functions and normalization of delta features

--- utils.py
+++ utils.py
@@ -37,13 +37,16 @@ def get_atoms_info(molecule):
         coordinates.append((position.x, position.y, position.z))
         if element_symbol != 'H':
             coordinates_no_H.append((position.x, position.y, position.z))
-        delta_neutrons.append(neutron_num - atomic_num)
+        if neutron_num - atomic_num < 0:    
+            delta_neutrons.append(0)
+        else:
+            delta_neutrons.append(neutron_num - atomic_num)
 
 
     coordinates = np.array(coordinates)
     coordinates_no_H = np.array(coordinates_no_H)
 
-    return elements, masses, protons, neutrons, electrons, coordinates, coordinates_no_H, formal_charges, delta_neutrons
+    return elements, masses, np.array(protons), np.array(neutrons), np.array(electrons), coordinates, coordinates_no_H, np.array(formal_charges), np.array(delta_neutrons)
 
 
 def molecule_info(molecule):
@@ -85,3 +88,62 @@ def normalize_features(info: dict):
 
     return info
 
+def normalize_delta_features(info: dict):
+    """
+    Normalizes the numbers of protons, delta_neutrons and formal_charges of a molecule 
+    using the range of the coordinates
+    """
+    # Get the maximum and minimum from the coordinates array
+    max = np.amax(info['coordinates'])
+    min = np.amin(info['coordinates'])
+
+    # Normalize the features
+    # Protons
+    p_min = np.amin(info['protons'])
+    p_max = np.amax(info['protons'])
+    info['protons'] = (info['protons'] - p_min) / (p_max - p_min) * (max - min) + min
+
+    # Delta neutrons
+    dn_min = np.amin(info['delta_neutrons'])
+    dn_max = np.amax(info['delta_neutrons'])
+    if dn_min < 0:
+        dn_min = 0
+    if dn_min != dn_max:
+        info['delta_neutrons'] = (info['delta_neutrons'] - dn_min) / (dn_max - dn_min) * (max - min) + min
+
+    # Formal charges
+    fc_min = np.amin(info['formal_charges'])
+    fc_max = np.amax(info['formal_charges'])
+    if fc_min != fc_max:
+        info['formal_charges'] = (info['formal_charges'] - fc_min) / (fc_max - fc_min) * (max - min) + min
+
+    return info
+
+def taper_features(info: dict):
+    """
+    Tapers the numbers of protons, neutrons and electrons of a molecule using a tapering function
+    """
+    info['protons'] = np.sqrt(info['protons'])
+    info['neutrons'] = np.sqrt(info['neutrons'])
+    info['electrons'] = np.sqrt(info['electrons'])
+
+    # info['protons'] = np.log(info['protons'] + 1)
+    # info['neutrons'] = np.log(info['neutrons'] + 1)
+    # info['electrons'] = np.log(info['electrons'] + 1)
+
+    return info
+
+def taper_delta_features(info: dict):
+    """
+    Tapers the number of protons, delta_neutrons and formal_charges of a molecule using a tapering function
+    """
+
+    # info['protons'] = np.sqrt(info['protons'])
+    # info['delta_neutrons'] = np.sqrt(info['delta_neutrons'])
+    # info['formal_charges'] = np.sqrt(info['formal_charges'])
+
+    info['protons'] = np.log(info['protons'] + 1)
+    info['delta_neutrons'] = np.log(info['delta_neutrons'] + 1)
+    info['formal_charges'] = np.log(info['formal_charges'] + 1)
+
+    return info
