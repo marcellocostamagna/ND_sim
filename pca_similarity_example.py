7d7951063cf6a850eb5e6a0a3ffe93cf82d14e17
Marcello Costamagna <marcello.costamagna@uib.no>
2023-06-20 11:40:43 +0200

    restore computation with covariance for comparison with SVD

--- pca_similarity_example.py
+++ pca_similarity_example.py
@@ -1,35 +1,59 @@
 # Script to test the capability of the pca_fingerprints and their comparison
 
 import numpy as np
-import matplotlib.pyplot as plt
 from perturbations import *
 from rdkit import Chem
 from utils import *
 from fingerprints import *
 from similarity_3d import *
-from copy import deepcopy
 from pca_fingerprint import *
 
 
-def compute_6D_similarity(query, target):
+def compute_6D_similarity_SVD(query, target):
     """Compute the similarity between two 6D fingerprints"""
 
-    # points = translate_points_to_geometrical_center(query['coordinates'])
-    # points1 = translate_points_to_geometrical_center(target['coordinates'])
+    # Features normalization
+    # query = taper_features(query)
+    #query = normalize_features(query)
+    #query = taper_features(query)
 
-    data = np.hstack((query['coordinates'], 
-                      np.array(query['protons']).reshape(-1, 1), 
-                      np.array(query['neutrons']).reshape(-1, 1),
-                      np.array(query['electrons']).reshape(-1, 1)))
+    # Delta features normalization
+    #query = taper_delta_features(query)
+    query = normalize_delta_features(query)
     
-    # rotate data
-    target['coordinates'] = rotate_points(target['coordinates'], 0, 0, 0)
+    # data = np.hstack((query['coordinates'], 
+    #                   np.array(query['protons']).reshape(-1, 1), 
+    #                   np.array(query['neutrons']).reshape(-1, 1),
+    #                   np.array(query['electrons']).reshape(-1, 1)))
     
-    data1 = np.hstack((target['coordinates'], 
-                       np.array(target['protons']).reshape(-1, 1),
-                       np.array(target['neutrons']).reshape(-1, 1),
-                       np.array(target['electrons']).reshape(-1, 1)))
+    data = np.hstack((query['coordinates'],
+                        np.array(query['protons']).reshape(-1, 1),
+                        np.array(query['delta_neutrons']).reshape(-1, 1),
+                        np.array(query['formal_charges']).reshape(-1, 1)))
+    
+    
+    # Rotate data
+    #target['coordinates'] = rotate_points(target['coordinates'], 0, 0, 0)
 
+    # Features normalization
+    # target = taper_features(target)
+    target = normalize_features(target)
+    # target = taper_features(target)
+
+    # Delta features normalization
+    # target = taper_delta_features(target)
+    #target = normalize_delta_features(target)
+    
+    # data1 = np.hstack((target['coordinates'], 
+    #                    np.array(target['protons']).reshape(-1, 1),
+    #                    np.array(target['neutrons']).reshape(-1, 1),
+    #                    np.array(target['electrons']).reshape(-1, 1)))
+    
+    data1 = np.hstack((target['coordinates'],
+                        np.array(target['protons']).reshape(-1, 1),
+                        np.array(target['delta_neutrons']).reshape(-1, 1),
+                        np.array(target['formal_charges']).reshape(-1, 1)))
+    
     fingerprint_query = get_pca_fingerprint(data)
     fingerprint_target = get_pca_fingerprint(data1)
 
@@ -41,10 +65,9 @@ def compute_6D_similarity(query, target):
 #suppl = Chem.SDMolSupplier('big_mol.sdf', removeHs=False)
 #suppl = Chem.SDMolSupplier('coumarins_test.sdf', removeHs=False)
 #suppl = Chem.SDMolSupplier('isomers_natural.sdf', removeHs=False)
-suppl = Chem.SDMolSupplier('isomers_test.sdf', removeHs=False)
-#suppl = Chem.SDMolSupplier('swapping.sdf', removeHs=False)
+#suppl = Chem.SDMolSupplier('isomers_test.sdf', removeHs=False)
+suppl = Chem.SDMolSupplier('swapping.sdf', removeHs=False)
 molecules = [mol for mol in suppl if mol is not None]
-#print(len(molecules))
 
 molecules_info = {}
 
@@ -52,50 +75,11 @@ for i, molecule in enumerate(molecules):
     info = molecule_info(molecule)
     molecules_info[f'molecule_{i}'] = info
 
-molecule_1 = molecules_info['molecule_4']
-molecule_2 = molecules_info['molecule_5']
+molecule_1 = molecules_info['molecule_0']
+molecule_2 = molecules_info['molecule_1']
 
-# similarity_4d = compute_4D_similarity(molecule_1, molecule_2)
-# similarity_5d = compute_5D_similarity(molecule_1, molecule_2)
-similarity_6d = compute_6D_similarity(molecule_1, molecule_2) 
+similarity_6d_SVD = compute_6D_similarity_SVD(molecule_1, molecule_2) 
+similarity_6d_cov = compute_6D_similarity_cov(molecule_1, molecule_2)
 
-# print(f'Similarity_4d: {similarity_4d}')
-# print(f'Similarity_5d: {similarity_5d}')
-print(f'Similarity_6d: {similarity_6d}')
+print(f'Similarity_6d: {similarity_6d_SVD}')
 
-
-
-
-
-
-
-
-#def compute_4D_similarity(query, target):
-#     """Compute the similarity between two 4D fingerprints"""
-
-#     # points = translate_points_to_geometrical_center(query['coordinates'])
-#     # points1 = translate_points_to_geometrical_center(target['coordinates'])
-
-#     # add the protons to the coordinates
-#     data = np.hstack((query['coordinates'], np.array(query['protons']).reshape(-1, 1)))
-#     data1 = np.hstack((target['coordinates'], np.array(target['protons']).reshape(-1, 1)))
-#     fingerprint_query = get_pca_fingerprint(data)
-#     fingerprint_target = get_pca_fingerprint(data1)
-
-#     similarity = 1/(1 + calculate_nD_partial_score(fingerprint_query, fingerprint_target))
-#     return similarity
-
-# def compute_5D_similarity(query, target):
-#     """Compute the similarity between two 5D fingerprints"""
-
-#     # points = translate_points_to_geometrical_center(query['coordinates'])
-#     # points1 = translate_points_to_geometrical_center(target['coordinates'])
-
-#     data = np.hstack((query['coordinates'], np.array(query['protons']).reshape(-1, 1), np.array(query['delta_neutrons']).reshape(-1, 1)))
-#     data1 = np.hstack((target['coordinates'], np.array(target['protons']).reshape(-1, 1), np.array(target['delta_neutrons']).reshape(-1, 1)))
-
-#     fingerprint_query = get_pca_fingerprint(data)
-#     fingerprint_target = get_pca_fingerprint(data1)
-
-#     similarity = 1/(1 + calculate_nD_partial_score(fingerprint_query, fingerprint_target))
-#     return similarity
