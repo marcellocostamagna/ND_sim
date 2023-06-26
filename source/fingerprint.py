71c20b7ad3aac8bac2d580882cc6108b351ac23b
Marcello Costamagna <marcello.costamagna@uib.no>
2023-06-26 16:30:36 +0200

    adds the fingerprint module of the similarity measure

---
+++ source/fingerprint.py
@@ -0,0 +1,48 @@
+# Script that provides the fingerprints of the molecules represented as a 6D array
+
+import numpy as np
+from scipy.spatial import distance
+from scipy.stats import skew
+
+def get_reference_points():
+    """
+    Returns the reference points
+    """
+    centroid = np.array([0, 0, 0, 0, 0, 0])
+    axis_points = np.eye(6)
+    reference_points = np.vstack((centroid, axis_points))
+    return reference_points
+
+def compute_distances(molecule_data: np.ndarray):
+    """
+    Computes the Euclidean distance of each point from each reference point
+    """
+    reference_points = get_reference_points()
+    distances = np.empty((molecule_data.shape[0], len(reference_points)))
+    for i, point in enumerate(molecule_data):
+        for j, ref_point in enumerate(reference_points):
+            distances[i, j] = distance.euclidean(point, ref_point)
+    return distances
+
+def compute_statistics(distances):
+    means = np.mean(distances, axis=1)
+    std_devs = np.std(distances, axis=1)
+    skewness = skew(distances, axis=1)
+    # check if skewness is nan
+    skewness[np.isnan(skewness)] = 0
+    
+    statistics_matrix = np.vstack((means, std_devs, skewness)).T 
+    # add all rows to a list   
+    statistics_list = [element for row in statistics_matrix for element in row]
+
+    return statistics_list  
+
+
+def get_fingerprint(molecule_data: np.ndarray):
+    """Computes the fingerprint of a given data set."""
+    # Compute the Euclidean distance of each point from each reference point (which are fixed)
+    distances = compute_distances(molecule_data)
+    # Compute the statistics of the distances (mean, std_dev, skewness)
+    fingerprint = compute_statistics(distances.T)
+    
+    return fingerprint
