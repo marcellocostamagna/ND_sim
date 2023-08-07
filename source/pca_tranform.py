816c0331c2837082c91eaf4b73e72ea2b5c67306
Marcello Costamagna <marcello.costamagna@uib.no>
2023-08-07 11:09:24 +0200

    adds implementation fro the eigendecomposition of the covarinace matrix and the non-aritrary choice of the eigenvectors sign

--- source/pca_tranform.py
+++ source/pca_tranform.py
@@ -44,4 +44,39 @@ def perform_PCA_and_get_transformed_data(original_data):
 
     
 def perform_PCA_and_get_transformed_data_cov(original_data):
-    return True
+    """
+    Performs the PCA analysis via the eigendecomposition of the covariance 
+    matrix to the n-dimensional data representing a molecule and returning 
+    the transformed data with which obtain the fingerprint, 
+    """
+    covariance_matrix = np.cov(original_data, ddof=0, rowvar=False)
+    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+
+    transformed_data = np.dot(original_data, eigenvectors)
+    
+    eigenvectors, transformed_data = choose_eig_sign(eigenvectors, transformed_data)
+
+    return original_data, transformed_data, eigenvectors, eigenvalues
+
+def choose_eig_sign(eigenvectors, transformed_data):
+    for i in range(eigenvectors.shape[1]):
+        remaining_indices = np.arange(transformed_data.shape[0])  # start considering all points
+        max_abs_coordinate = np.max(np.abs(transformed_data[:, i]))
+
+        while True:
+            # find the points with maximum absolute coordinate among the remaining ones
+            mask_max = np.isclose(np.abs(transformed_data[remaining_indices, i]), max_abs_coordinate)
+            max_indices = remaining_indices[mask_max]  # indices of points with maximum absolute coordinate
+
+            if len(max_indices) == 1:  # we found a single unambiguous point
+                break
+            
+            # if there is a tie, ignore these points and find the maximum absolute coordinate again
+            remaining_indices = remaining_indices[~mask_max]
+            max_abs_coordinate = np.max(np.abs(transformed_data[remaining_indices, i]))
+        
+        if transformed_data[max_indices[0], i] < 0:
+            eigenvectors[:, i] *= -1
+            transformed_data[:, i] *= -1
+
+    return eigenvectors, transformed_data
