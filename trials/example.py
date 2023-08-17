7e978ef22c77b73d6f55cc499741bebe0cf332cc
Marcello Costamagna <marcello.costamagna@uib.no>
2023-08-17 16:30:45 +0200

    first update of example script

--- trials/example.py
+++ trials/example.py
@@ -3,34 +3,35 @@ from similarity.source.pre_processing import *
 from similarity.source.pca_tranform import * 
 from similarity.source.fingerprint import *
 from similarity.source.similarity import *
+from similarity.source.utils import *
 import os 
 
 cwd = os.getcwd()
 # PRE-PROCESSING
 # List of molecules from SDF file
 molecules = collect_molecules_from_sdf(f'{cwd}/similarity/sd_data/swapping.sdf')
-# List of dictionaries containing information about each molecule
-molecules_info = [collect_molecule_info(molecule) for molecule in molecules]
-# Manipulation of molecules_info: Normalization or Tapering
-# Normalization
-# First normalization
-#molecules_info = [normalize_features(molecule) for molecule in molecules_info] 
-# Second normalization
-#molecules_info = [normalize_features2(molecule) for molecule in molecules_info]
-# Tapering
-molecules_info = [taper_features(molecule, np.log) for molecule in molecules_info]
-# Get the 6D matrix for each molecule
-molecules_data = [get_molecule_6D_datastructure(molecule_info) for molecule_info in molecules_info]
+
+# OPTONAL
+# Define new fetaures function and respective rescaling functions to store
+# into a features dictionary
+
+# Get molecules represenation based on features expressed by the user
+molecules_data = [mol_nd_data(mol) for mol in molecules]
 
 # PCA
 # Get the PCA tranformed data 
-_, tranformed_data, _, _ = perform_PCA_and_get_transformed_data_cov(molecules_data[2])
+_, transformed_data, _, _ = perform_PCA_and_get_transformed_data_cov(molecules_data[2])
 _, transformed_data1, _, _ = perform_PCA_and_get_transformed_data_cov(molecules_data[3])
 
 # FINGERPRINT
-# Get the fingerprint from teh tranformed data
-fingerprint  = get_fingerprint(tranformed_data)
-fingerprint1 = get_fingerprint(transformed_data1)
+# OPTIONAL
+# Define a scaling factor of a scaling matrix to modify the reference points
+scaling_factor = compute_scaling_factor(transformed_data)
+scaling_factor1 = compute_scaling_factor(transformed_data1)
+
+# Get the fingerprint from the tranformed data
+fingerprint  = get_fingerprint(transformed_data, scaling_factor)
+fingerprint1 = get_fingerprint(transformed_data1, scaling_factor1)
 
 # SIMILARITY
 # Calculate the similarity between two fingerprints
