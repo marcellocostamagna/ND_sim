b95c4d3341ecdc3013241281ac500fa73ed775fd
Marcello Costamagna <marcello.costamagna@uib.no>
2023-09-06 11:13:45 +0200

    modifies example to compute and print the similarity between all the pairs in a molecule file

--- trials/example.py
+++ trials/example.py
@@ -6,36 +6,42 @@ from similarity.source.similarity import *
 from similarity.source.utils import *
 import os 
 
+np.set_printoptions(precision=4, suppress=True)
+
 cwd = os.getcwd()
 # PRE-PROCESSING
 # List of molecules from SDF file
-molecules = collect_molecules_from_sdf(f'{cwd}/similarity/sd_data/swapping.sdf')
+molecules = collect_molecules_from_sdf(f'{cwd}/similarity/sd_data/swapping.sdf', removeHs=False)
 
-# OPTONAL
-# Define new fetaures function and respective rescaling functions to store
-# into a features dictionary
-
-# Get molecules represenation based on features expressed by the user
+# Get molecules representaTion based on features expressed by the user
 molecules_data = [mol_nd_data(mol) for mol in molecules]
 
-# PCA
-# Get the PCA tranformed data 
-_, transformed_data, _, _ = perform_PCA_and_get_transformed_data_cov(molecules_data[2])
-_, transformed_data1, _, _ = perform_PCA_and_get_transformed_data_cov(molecules_data[3])
+# print molecules_data
+# for i in range(len(molecules_data)):
+#     print(f"molecule {i+1}: \n {molecules_data[i]}")
 
-# FINGERPRINT
-# OPTIONAL
-# Define a scaling factor of a scaling matrix to modify the reference points
-scaling_factor = compute_scaling_factor(transformed_data)
-scaling_factor1 = compute_scaling_factor(transformed_data1)
+fingerprints = []
+for j, molecule_data in enumerate(molecules_data):
+    # PCA
+    # Get the PCA tranformed data 
+    _, transformed_data, _, _ = perform_PCA_and_get_transformed_data_cov(molecule_data)
+    # print(f"molecule {j+1} : \n {transformed_data}")
 
-# Get the fingerprint from the tranformed data
-fingerprint  = get_fingerprint(transformed_data, scaling_factor)
-fingerprint1 = get_fingerprint(transformed_data1, scaling_factor1)
+    # FINGERPRINT
+    # OPTIONAL
+    # Define a scaling factor of a scaling matrix to modify the reference points
+    # scaling_factor = compute_scaling_factor(transformed_data)
+    # scaling_matrix = compute_scaling_matrix(transformed_data) 
+    scaling_factor = 1
 
-# SIMILARITY
-# Calculate the similarity between two fingerprints
-partial_score = calculate_partial_score(fingerprint, fingerprint1) # Manhattan distance
-similarity = get_similarity_measure(partial_score)
+    # Get the fingerprint from the tranformed data
+    fingerprint  = get_fingerprint(transformed_data, scaling_factor)
+    fingerprints.append(fingerprint)
 
-print(similarity)
+# Compute similarity between all pairs of fingerprints
+n_molecules = len(fingerprints)
+for i in range(n_molecules):
+    for j in range(i+1, n_molecules):
+        partial_score = calculate_partial_score(fingerprints[i], fingerprints[j])
+        similarity = get_similarity_measure(partial_score)
+        print(f"{i+1}-{j+1}: {similarity}")
