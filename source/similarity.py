77dd946ba738b99d079e7b2d06693a5964ff5312
Marcello Costamagna <marcello.costamagna@uib.no>
2023-09-18 14:23:38 +0200

    adds function to directly compute similarity from to rdkit mol

--- source/similarity.py
+++ source/similarity.py
@@ -1,4 +1,6 @@
 # Script to compare two fingerprints and provide a single-value similarity measure
+from similarity.source.utils import * 
+from similarity.source.fingerprint import *
 
 def calculate_partial_score(moments1: list, moments2:list):
     partial_score = 0
@@ -14,4 +16,11 @@ def get_similarity_score(fingerprint_1, fingerprint_2):
     similarity = get_similarity_measure(partial_score)
     return similarity
 
-  
+def compute_similarity(mol1, mol2, features=DEFAULT_FEATURES, scaling_method='matrix'):
+    # Get molecules' fingerprints
+    f1 = get_nd_fingerprint(mol1, features=features, scaling_method=scaling_method)
+    f2 = get_nd_fingerprint(mol2, features=features, scaling_method=scaling_method)
+    # Compute similarity score
+    similarity_score = get_similarity_score(f1, f2)
+    return similarity_score
+
