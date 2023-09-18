c73b3672e0312d0e06deb7a3e11bb4ab74476af3
Marcello Costamagna <marcello.costamagna@uib.no>
2023-09-18 14:10:32 +0200

    improves eigenvectors sign choice function

--- source/pca_tranform.py
+++ source/pca_tranform.py
@@ -10,35 +10,42 @@ def perform_PCA_and_get_transformed_data_cov(original_data):
     matrix to the n-dimensional data representing a molecule and returning 
     the transformed data with which obtain the fingerprint, 
     """
-    covariance_matrix = np.cov(original_data, ddof=0, rowvar=False)
+    covariance_matrix = np.cov(original_data, rowvar=False, ddof=0,)
     eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
     eigenvalues, eigenvectors = eigenvalues[::-1], eigenvectors[:, ::-1]
 
     eigenvectors = choose_eig_sign(original_data, eigenvectors)
+    #print(f"eigenvectors: \n {eigenvectors}")
 
     transformed_data = np.dot(original_data, eigenvectors)
 
     return original_data, transformed_data, eigenvectors, eigenvalues
 
 
-def choose_eig_sign(original_data, eigenvectors, tolerance=1e-4):
+def choose_eig_sign(original_data, eigenvectors, tolerance= 1e-4):
     for i in range(eigenvectors.shape[1]):
         # Compute the projections of the original data onto the current eigenvector
         projections = original_data.dot(eigenvectors[:, i])
-        sorted_indices = np.argsort(np.abs(projections))[::-1]
-        # Determine the largest magnitude projection
-        largest_projection = projections[sorted_indices[0]]
+
+        remaining_indices = np.arange(original_data.shape[0])  # start considering all points
+        max_abs_coordinate = np.max(np.abs(projections))
+
+        while True:
+            # find the points with maximum absolute coordinate among the remaining ones
+            mask_max = np.isclose(np.abs(projections[remaining_indices]), max_abs_coordinate, atol=tolerance)
+            max_indices = remaining_indices[mask_max]  # indices of points with maximum absolute coordinate
+
+            if len(max_indices) == 1:  # we found a single unambiguous point
+                break
+            
+            # if there is a tie, ignore these points and find the maximum absolute coordinate again
+            remaining_indices = remaining_indices[~mask_max]
+            if len(remaining_indices) == 0: # if all points have the same component, break the loop
+                break
+            max_abs_coordinate = np.max(np.abs(projections[remaining_indices]))
         
-        # Check for ambiguities and resolve them
-        for idx in sorted_indices:
-            if np.isclose(np.abs(projections[idx]), np.abs(largest_projection), atol=tolerance):
-                if projections[idx] < 0: 
-                    largest_projection = projections[idx]  
-                    break
-            else:
-                break  
-        # Update the direction of the eigenvector based on the sign of the largest magnitude projection
-        if largest_projection < 0:
+        if len(remaining_indices) > 0 and projections[max_indices[0]] < 0:
             eigenvectors[:, i] *= -1
 
-    return eigenvectors
+    return eigenvectors 
+
